bff45afdb7dac3c526d6121a85ded5a23d110c8f
claudiodavic <123840391+claudiodavic@users.noreply.github.com>
2023-03-01 10:54:16 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,4 @@
+function cambia_de_pagina(){
+    alert("Gracias por visitar mi Cv Web, nos vamos con una imagen final. Saludos cordiales.")
+    location.href="https://www.eldinamo.cl/cdn-cgi/mirage/764c1f0e42124f53b2294e832e262765dc09ee4f9e3167893374f24215b65105/1280/https://static.eldinamo.cl/media/2022/12/18-190110_e452_arg-campeon-880x500.jpg"
+}
